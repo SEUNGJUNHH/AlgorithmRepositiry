de6f9f6afb8332ea70b49eea7839ec3d7dfabdd1
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2023-04-30 21:02:24 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.45 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        int count =0;
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j*j <= i; j++) {
+              if(j*j==i)count++;
+              else if (i%j==0) {
+                  count+=2;
+              }
+            }
+            if(count>limit)answer+=power;
+            else answer+=count;
+            count = 0;
+        }
+        return answer;
+    }
+}
