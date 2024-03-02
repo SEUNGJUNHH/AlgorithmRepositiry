7f5a743a97ca9312bd8753ef1ba7b72e5521c18d
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2024-03-02 16:31:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2192 ms, Memory: 61212 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static class XY{
+        int x;
+        int y;
+
+        public XY(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n  = Integer.parseInt(br.readLine());
+        XY[] xy = new XY[n];
+        for (int i = 0; i < n; i++) {
+            XY a = new XY(0,0);
+            st = new StringTokenizer(br.readLine());
+            a.x= Integer.parseInt(st.nextToken());
+            a.y= Integer.parseInt(st.nextToken());
+            xy[i] = a;
+        }
+        Arrays.sort(xy,(a,b)->{
+            if(a.x==b.x){
+                return a.y-b.y;
+            }else{
+                return a.x-b.x;
+            }
+        });
+        for (XY xy1 : xy) {
+            System.out.print(xy1.x+" "+xy1.y);
+            System.out.println();
+        }
+        //index는 1부터
+        br.close();
+    }
+
+
+}
