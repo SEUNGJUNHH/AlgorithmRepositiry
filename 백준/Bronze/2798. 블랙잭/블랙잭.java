8c6ef6b84938caa7f4c46571c6cae356c9672707
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2024-05-17 14:12:51 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int answer = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                for (int k = j+1; k < arr.length; k++) {
+                    int num = arr[i]+arr[j]+arr[k];
+                    if(num>M) break;
+                    else if (M-num<M-answer) {
+                        answer = num;
+                    }
+                }
+
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
