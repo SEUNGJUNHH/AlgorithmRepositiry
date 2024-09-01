2cb320ac70204c216bbc581f8eeaf2e225d7f7e6
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2024-09-01 16:21:05 +0900

    [level 1] Title: K번째수, Time: 0.31 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,23 @@
 import java.util.*;
-class Solution {
-    int first, end, point;
-    
-    
+class Solution {    
     public int[] solution(int[] array, int[][] commands) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int row = commands.length;
-        int[] answer = new int[row];
-        for(int i = 0 ; i<row;i++){
-            list.clear();
-            first = commands[i][0]-1;
-            end = commands[i][1]-1;
-            point = commands[i][2]-1;
-            for(int j =first;j<=end;j++){
-                list.add(array[j]);
+        ArrayList<Integer> list = new ArrayList<>();
+        //commands 길이 구하는 반복문
+        for(int i =0; i<commands.length;i++){
+            //각 구간별 값 저장하는 list
+            ArrayList<Integer> save = new ArrayList<>();
+            // 3번 반복해서 각 point 선정
+            for(int j = commands[i][0]-1 ; j<=commands[i][1]-1; j++){
+                save.add(array[j]);
             }
-            Collections.sort(list);
-            answer[i] = list.get(point);
-            
+            Collections.sort(save);
+            list.add(save.get(commands[i][2]-1));
         }
-        return answer;
+        
+        int[] arr = new int[list.size()];
+        for(int i =0; i< list.size();i++){
+            arr[i] = list.get(i);
+        }
+        return arr;
     }
 }
