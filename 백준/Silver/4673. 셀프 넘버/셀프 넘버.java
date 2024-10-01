896d94ad764e0bff1e3a251f135c5c08e8626dc6
SEUNGJUNHH <103011980+SEUNGJUNHH@users.noreply.github.com>
2024-10-01 16:38:22 +0900

    [Silver V] Title: 셀프 넘버, Time: 128 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,31 +1,23 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        final int MAX = 10000; // 상수로 최대 값 정의
-        boolean[] arr = new boolean[MAX + 1]; // 1부터 10000까지 배열 생성
-
-        // 1부터 MAX까지 각 숫자의 셀프 넘버 여부 판단
-        for (int i = 1; i <= MAX; i++) {
-            int generator = getGenerator(i); // 생성자 값 계산
-            if (generator <= MAX) arr[generator] = true; // 배열에 표시
+        boolean[] arr = new boolean[10001];
+        for (int i = 1; i < arr.length; i++) {
+            int number = i;
+            int answer = i;
+            while (number > 0) {
+                int digit = number % 10; // 마지막 자리수 추출
+                answer+=digit;
+                number /= 10; // 숫자를 10으로 나눠 다음 자리수로 이동
+            }
+            if(answer<=10000) arr[answer] = true;
         }
-
-        // 셀프 넘버 출력 (arr[i]가 false인 경우)
-        for (int i = 1; i <= MAX; i++) {
-            if (!arr[i]) System.out.println(i);
+        for (int i = 1; i < arr.length; i++) {
+            if(!arr[i]) System.out.println(i);
         }
-    }
-
-    // d(n)을 계산하는 함수
-    public static int getGenerator(int n) {
-        int sum = n;
-        while (n > 0) {
-            sum += n % 10; // 마지막 자리수 더하기
-            n /= 10; // 다음 자리수로 이동
-        }
-        return sum;
     }
 }
